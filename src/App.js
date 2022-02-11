441808f6fb4ff0af4f700a5a22f32ff1c7015cec
Daniel <daniel@codings.dk>
2022-02-11 10:31:35 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/Navbar";
+import ViewTransactions from "./pages/ViewTransactions";
+import CreateTransaction from "./pages/CreateTransaction";
+import axios from "axios";
 
-import Navbar from './pages/Navbar';
-
-import ViewTransactions from './pages/ViewTransactions';
-import CreateTransaction from './pages/CreateTransaction';
-
-import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = "https://localhost:8080/api/";
 
 function App() {
   return (
-      <div className="App">
-          <Router>
-              <Navbar />
-              <Routes>
-                  <Route path="/view" element={ViewTransactions} />
-                  <Route path="/creat" element={CreateTransaction} />
-              </Routes>
-          </Router>
-      </div>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/view" element={<ViewTransactions />} />
+          <Route path="/create" element={<CreateTransaction />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/ViewTransactions.jsx
+++ src/pages/ViewTransactions.jsx
@@ -1,77 +1,64 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-function ViewTransaction() {
+const ViewTransaction = () => {
+  const [loading, setLoading] = useState(true);
+  const [transactions, setTransaction] = useState([]);
 
-    const [loading, setLoading] = useState(true);
-    const [transactions, setTransaction] = useState([]);
+  useEffect(() => {
+    axios.get(`/api`).then((res) => {
+      if (res.status === 200) {
+        setTransaction(res.data.transactions);
+        setLoading(false);
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`/api`).then(res=>{
-            if(res.status === 200)
-            {
-                setTransaction(res.data.transactions)
-                setLoading(false);
-            }
-        });
+  if (loading) {
+    return <h4>Loading Transactions...</h4>;
+  }
 
-    }, []);
-
-    if(loading) {
-        return <h4>Loading Transactions...</h4>
-    }
-
-    // else {
-    //     var transaction_HTMLTABLE = "";
-    //     transaction_HTMLTABLE = transactions.map( (item, index) => {
-    //         return (
-    //             <tr key={index}>
-    //                 <td>{item.id}</td>
-    //                 <td>{item.description}</td>
-    //                 <td>{item.amount}</td>
-    //                 <td>{item.receipt_date}</td>
-    //             </tr>
-    //         );
-    //     });
-    }
-
-    return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h4>Transaction Data
-                            <Link to={'CreateTransaction'} className="btn btn-primary btn-sm float-end"> Create Transaction</Link>
-                        </h4>
-                    </div>
-                    <div className="card-body">
-                        <table className="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Description</th>
-                                    <th>Amount</th>
-                                    <th>Receipt Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {/*{transaction_HTMLTABLE}*/}
-
-                                transactions.map( (item, index) => {
-                                    return (
-                                    <tr key={index}>
-                                    <td>{item.id}</td>
-                                    <td>{item.description}</td>
-                                    <td>{item.amount}</td>
-                                    <td>{item.receipt_date}</td>
-                                    </tr>
-                                    )});
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-    );
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h4>
+            Transaction Data
+            <Link
+              to={"CreateTransaction"}
+              className="btn btn-primary btn-sm float-end"
+            >
+              {" "}
+              Create Transaction
+            </Link>
+          </h4>
+        </div>
+        <div className="card-body">
+          <table className="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Receipt Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>{item.description}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.receipt_date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ViewTransaction;
